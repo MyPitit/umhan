70f1474bed0ba74c059807d509e85b0f92fcc753
Mireia <mireia@mireiasangalo.com>
2016-04-29 10:08:27 +0100

    modal dropmenu and readme

--- frontend/src/js/views/Home/home_index.js
+++ frontend/src/js/views/Home/home_index.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Grid, Row, Col, ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { getUserDetails } from '../../actions/actions_index.js'
 
+export default (props) => {
+  const userList = (profile) => {
+    props.changeState({
+      userSelected: profile
+    })
+  }
+}
 class Home extends React.Component {
   render () {
     console.log(this.props)
@@ -10,11 +17,21 @@ class Home extends React.Component {
       <Grid className='home'>
         <Row>
           <Col xs={12}>
-            <div className='image-container'>
-              <img src='img/logo.png' />
+            <div className='home'>
+              <h3>Please select institution</h3>
             </div>
-            <p>Your app goes here...</p>
-            <a href='/login-with-twitter'>login with twitter</a>
+            <div className='home'>
+              <ButtonToolbar>
+                <DropdownButton bsSize='large' title='Select institution' id='dropdown-size-large'>
+                  <MenuItem eventKey='1' onClick={() => user(props.profile) }>Birmingham City University</MenuItem>
+                  <MenuItem eventKey='2' onClick={() => user(props.profile) }>Blackburn College</MenuItem>
+                  <MenuItem eventKey='3' onClick={() => user(props.profile) }>Buckinghamshire</MenuItem>
+                  <MenuItem eventKey='4' onClick={() => user(props.profile) }>Cambridge University</MenuItem>
+                  <MenuItem eventKey='5' onClick={() => user(props.profile) }>Canterbury Christ Church University</MenuItem>
+                  <MenuItem eventKey='6' onClick={() => user(props.profile) }>City University</MenuItem>
+                </DropdownButton>
+              </ButtonToolbar>
+            </div>
           </Col>
         </Row>
       </Grid>
